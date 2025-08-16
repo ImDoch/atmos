f83227df71ace460e50a447333c4217f2b50af21
Jeison Gomez <gomezres011@gmail.com>
2025-08-16 14:54:22 -0500

    estilos para boton de volver atras, se elimina modal para busqueda en moviles

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
-const mobileSearchButton = document.querySelector('.search__button--mobile')
-const mobileSearchModal = document.querySelector('.mobile-search')
-const mobileCloseModal = document.querySelector('.close-modal')
 const toggleModeButton = document.querySelector('.toggle-mode-button')
 
-mobileSearchButton.addEventListener('click', () => {
-    mobileSearchModal.showModal()
-})
-mobileCloseModal.addEventListener('click', () => { 
-    mobileSearchModal.close()
-})
 toggleModeButton.addEventListener('click', () => { 
     document.body.classList.toggle('light')
 })
